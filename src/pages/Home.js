d5fe784bf382c255e9b95862fb7f9b6682a68103
franco <DomingosManuelFranco>
2021-07-15 12:49:24 +0100

    home page done

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+
+import illustration from '../assets/illustration.svg';
+
+export const Home = () => {
+
+    return (
+
+        <div>
+            <img src={illustration} alt="talking" />
+            <h1>Ask me any thing</h1>
+            <p>When you ask questions you learn. So don't be shy.</p>
+        </div>
+    )
+}
