73b6dc9bbb1255d4099bd318a7b347852d790a99
franco <DomingosManuelFranco>
2021-07-15 16:43:32 +0100

    home page styled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,26 @@
 
 import illustration from '../assets/illustration.svg';
+import logo from '../assets/logo.svg';
+import icon from '../assets/icon.svg';
 
 export const Home = () => {
 
     return (
 
         <div>
-            <img src={illustration} alt="talking" />
-            <h1>Ask me any thing</h1>
-            <p>When you ask questions you learn. So don't be shy.</p>
+            <div>
+                <img src={illustration} alt="talking" />
+                <h1>Ask me any thing</h1>
+                <p>When you ask questions you learn. So don't be shy.</p>
+            </div>
+
+            <div>
+                <img src={logo} alt="Ask me" />
+                <button>
+                    <img src={icon} alt="google Icon" />
+                    Login with Google to create a Room
+                </button>
+            </div>
         </div>
     )
 }
